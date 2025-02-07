5ba5c0c90815fb342294dc6024f4d8cc1e5b15d1
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-02-07 14:11:33 +0100

    add func

--- 4_func/app.py
+++ 4_func/app.py
@@ -1,7 +1,8 @@
 import asyncio
+from dataclasses import dataclass
 
 import logfire
-from pydantic_ai import Agent
+from pydantic_ai import Agent, RunContext
 from pydantic_ai.messages import ModelResponse, ModelRequest
 from pydantic_ai.models.openai import OpenAIModel
 from devtools import debug
@@ -9,8 +10,34 @@ import streamlit as st
 
 logfire.configure(send_to_logfire='if-token-present')
 
-model_name = "gemma2:9b"
-# model_name = "llama3.2:3b"
+model_name = "llama3.1:latest"
+
+
+class DatabaseConn:
+    """This is a fake database for example purposes.
+
+    In reality, you'd be connecting to an external database
+    (e.g. PostgreSQL) to get information about customers.
+    """
+
+    @classmethod
+    async def customer_name(cls, *, id: int) -> str | None:
+        if id == 123:
+            return 'John'
+
+    @classmethod
+    async def customer_balance(cls, *, id: int, include_pending: bool) -> float:
+        if id == 123:
+            return 123.45
+        else:
+            raise ValueError('Customer not found')
+
+
+@dataclass
+class SupportDependencies:
+    customer_id: int
+    db: DatabaseConn
+
 
 agent = Agent(
     OpenAIModel(
@@ -18,11 +45,26 @@ agent = Agent(
         base_url='http://localhost:11434/v1',
         api_key='your-api-key',
     ),
+    deps_type=SupportDependencies,
     retries=2,
 )
 
 
+@agent.tool
+async def customer_balance(
+        ctx: RunContext[SupportDependencies], include_pending: bool
+) -> str:
+    """Returns the customer's current account balance."""
+    balance = await ctx.deps.db.customer_balance(
+        id=ctx.deps.customer_id,
+        include_pending=include_pending,
+    )
+    return f'${balance:.2f}'
+
+
 async def main():
+    deps = SupportDependencies(customer_id=123, db=DatabaseConn())
+
     st.set_page_config(page_title="Local AI Chat", layout="wide")
     st.title("💬 Local AI Chatbot")
 
@@ -44,6 +86,7 @@ async def main():
 
         result = await agent.run(
             user_prompt=user_input,
+            deps=deps,
         )
         debug(result)
         st.session_state.history = result.all_messages()
