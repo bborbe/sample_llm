cecd12608ec3f13095400fa350340b6c6f94586b
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-02-24 16:06:40 +0100

    add docling examples

---
+++ 700_docling/app.py
@@ -0,0 +1,115 @@
+import os
+from typing import List
+from devtools import debug
+import asyncio
+import lancedb
+import logfire
+from docling.chunking import HybridChunker
+from docling.document_converter import DocumentConverter
+from dotenv import load_dotenv
+from lancedb.embeddings import get_registry
+from lancedb.pydantic import LanceModel, Vector
+from openai import OpenAI
+from tokenizer import OpenAITokenizerWrapper
+
+logfire.configure(send_to_logfire='if-token-present')
+load_dotenv()
+
+base_url = os.getenv('BASE_URL', 'http://localhost:11434/v1')
+api_key = os.getenv('API_KEY', 'no-llm-api-key-provided')
+model = os.getenv('MODEL', 'llama3.2:3b')
+embedding_model = os.getenv('EMBEDDING_MODEL', 'nomic-embed-text')
+embedding_dims = os.getenv('EMBEDDING_DIMS', 768)
+
+openai_client = OpenAI(
+    base_url=base_url,
+    api_key=api_key,
+)
+
+tokenizer = OpenAITokenizerWrapper()  # Load our custom tokenizer for OpenAI
+MAX_TOKENS = 8191  # text-embedding-3-large's maximum context length
+
+converter = DocumentConverter()
+
+chunker = HybridChunker(
+    tokenizer=tokenizer,
+    max_tokens=MAX_TOKENS,
+    merge_peers=True,
+)
+
+uri = "data/lancedb"
+db = lancedb.connect(uri)
+
+
+# Define a simplified metadata schema
+class ChunkMetadata(LanceModel):
+    """
+    You must order the fields in alphabetical order.
+    This is a requirement of the Pydantic implementation.
+    """
+
+    filename: str | None
+    page_numbers: List[int] | None
+    title: str | None
+
+
+func = get_registry().get("openai").create(
+    name="nomic-embed-text",
+    dim=embedding_dims,
+    base_url=base_url,
+    api_key=api_key,
+)
+
+
+class Chunks(LanceModel):
+    text: str = func.SourceField()
+    vector: Vector(embedding_dims) = func.VectorField()
+    metadata: ChunkMetadata
+
+
+def index():
+    result = converter.convert("https://arxiv.org/pdf/2408.09869")
+    chunk_iter = chunker.chunk(dl_doc=result.document)
+    chunks = list(chunk_iter)
+
+    processed_chunks = [
+        {
+            "text": chunk.text,
+            "metadata": {
+                "filename": chunk.meta.origin.filename,
+                "page_numbers": [
+                                    page_no
+                                    for page_no in sorted(
+                        set(
+                            prov.page_no
+                            for item in chunk.meta.doc_items
+                            for prov in item.prov
+                        )
+                    )
+                                ]
+                                or None,
+                "title": chunk.meta.headings[0] if chunk.meta.headings else None,
+            },
+        }
+        for chunk in chunks
+    ]
+
+    table = db.create_table("docling", schema=Chunks, mode="overwrite")
+    table.add(processed_chunks)
+    table.to_pandas()
+    table.count_rows()
+
+
+def search():
+    table = db.open_table("docling")
+    result = table.search(query="pdf").limit(5)
+    debug(result.to_pandas())
+
+
+async def main():
+    index()
+    search()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
